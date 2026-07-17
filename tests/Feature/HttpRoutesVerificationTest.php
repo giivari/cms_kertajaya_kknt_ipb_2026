c5332c2c6d5d312cad86db58ab77dd22e56ad114
givari <givari.zacky@gmail.com>
2026-07-17 15:41:58 +0700

    test: add HTTP routing verification coverage

---
+++ tests/Feature/HttpRoutesVerificationTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Admin;
+use Tests\TestCase;
+
+class HttpRoutesVerificationTest extends TestCase
+{
+    public function test_public_routes_return_200()
+    {
+        $this->get('/')->assertStatus(200);
+        $this->get('/berita')->assertStatus(200);
+        $this->get('/galeri')->assertStatus(200);
+        $this->get('/dokumen')->assertStatus(200);
+    }
+    
+    public function test_non_existent_route_returns_404()
+    {
+        $this->get('/this-route-does-not-exist-12345')->assertStatus(404);
+    }
+    
+    public function test_unauthenticated_admin_dashboard_redirects_to_login()
+    {
+        $response = $this->get('/test-admin-path');
+        $response->assertRedirect('/test-admin-path/login');
+    }
+}
