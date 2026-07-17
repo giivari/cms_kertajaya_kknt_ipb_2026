6181ddad512835f017ee1f944257d666b53f4cbe
givari <givari.zacky@gmail.com>
2026-07-17 16:09:58 +0700

    test: finalize phase one verification coverage

--- tests/Feature/HttpRoutesVerificationTest.php
+++ tests/Feature/HttpRoutesVerificationTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Feature;
 
-use App\Models\Admin;
 use Tests\TestCase;
 
 class HttpRoutesVerificationTest extends TestCase
@@ -14,12 +13,12 @@ class HttpRoutesVerificationTest extends TestCase
         $this->get('/galeri')->assertStatus(200);
         $this->get('/dokumen')->assertStatus(200);
     }
-    
+
     public function test_non_existent_route_returns_404()
     {
         $this->get('/this-route-does-not-exist-12345')->assertStatus(404);
     }
-    
+
     public function test_unauthenticated_admin_dashboard_redirects_to_login()
     {
         $response = $this->get('/test-admin-path');
